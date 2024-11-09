922fc3997bb706d4bc0688a66eaf999803120677
MonojitBairagi <monojitbairagi0@gmail.com>
2024-11-09 17:13:32 +0530

    changing in otp send

--- backend/controller/sendmail.js
+++ backend/controller/sendmail.js
@@ -15,11 +15,11 @@ async function sendEmail(otp, email) {
     subject: "Your OTP Verification Code",
     html: `
       <div style="max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e0e0e0; font-family: Arial, sans-serif;">
-        <h2 style="text-align: center; color: #4CAF50;">Aura Tracker OTP Verification</h2>
+        <h2 style="text-align: center; color: black;">Aura Tracker OTP Verification</h2>
         <p style="font-size: 16px; color: #555;">Hello,</p>
         <p style="font-size: 16px; color: #555;">Thank you for using our service! Please use the following One-Time Password (OTP) to verify your account:</p>
         <div style="text-align: center; margin: 20px 0;">
-          <span style="font-size: 24px; font-weight: bold; color: #4CAF50;">${otp}</span>
+          <span style="font-size: 24px; font-weight: bold; color: black;">${otp}</span>
         </div>
         <p style="font-size: 16px; color: #555;">This OTP is valid for only 2 minutes. Please do not share it with anyone for security purposes.</p>
         <p style="font-size: 16px; color: #555;">Best regards,<br>Your Aura Tracker Team</p>
